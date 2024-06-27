f9803ee7391ce51493b65135026d09de51503815
John Owen <john.owen@dabble.com.au>
2024-06-27 19:39:05 +1000

    tweaks

--- addons/temple/fps_controller/fps_controller.gd
+++ addons/temple/fps_controller/fps_controller.gd
@@ -2,29 +2,40 @@
 class_name FpsController
 extends RigidBody3D
 
+@export var standing_force_p_gain := 100.0
+@export var standing_force_d_gain := 10.0
+@export var upright_force_p_gain := 30.0
+@export var upright_force_d_gain := 6.0
+@export var turn_force_p_gain := 71.0
+@export var turn_force_d_gain := 35.0
+@export var ground_friction_coefficient := 20.0
+@export var walk_acceleration := 35.0
+@export var walk_speed_max := 6.0
+@export var walk_overspeed_deceleration := 2.0
 
-@export var ground_clearance: float = 0.3:
+
+@export var ground_clearance := 0.3:
 	set(value):
 		ground_clearance = value
 		if not is_node_ready():
-			return
-		_update()
+			await ready
+		_update_tree()
 
 
-@export var height: float = 1.7:
+@export var height := 1.7:
 	set(value):
 		height = value
 		if not is_node_ready():
-			return
-		_update()
+			await ready
+		_update_tree()
 
 
-@export var radius: float = 0.6:
+@export var radius := 0.6:
 	set(value):
 		radius = value
 		if not is_node_ready():
-			return
-		_update()
+			await ready
+		_update_tree()
 
 
 var _last_force_error: float
@@ -45,14 +56,14 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if Engine.is_editor_hint():
 		return
-	var is_on_ground := _physics_process_force_pid(delta)
-	_physics_process_walking(is_on_ground)
-	_physics_process_upright_torque_pid(delta)
-	_physics_process_yaw_torque_pid(delta)
+	var is_on_ground := _physics_process_standing_force(delta)
+	_physics_process_walking_force(is_on_ground)
+	_physics_process_upright_force(delta)
+	_physics_process_turn_force(delta)
 	camera.global_position = camera_anchor.global_position
 
 
-func _physics_process_force_pid(delta: float) -> bool:
+func _physics_process_standing_force(delta: float) -> bool:
 	var query := PhysicsRayQueryParameters3D.new()
 	var offset := 0.1
 	query.from = global_position + Vector3(0.0, ground_clearance + offset, 0.0)
@@ -67,18 +78,17 @@ func _physics_process_force_pid(delta: float) -> bool:
 
 	var error := ground_clearance - distance
 	var error_delta := (error - _last_force_error) / delta
-	var kp := 8000.0
-	var max_force := 8000.0
-	var td := 0.1
-	var up_force := minf(kp * (error + td * error_delta), max_force)
+	var up_accel := (
+		standing_force_p_gain * error + standing_force_d_gain * error_delta
+	)
 	var is_on_ground := error > 0.0
 	if is_on_ground:
-		apply_central_force(Vector3.UP * up_force)
+		apply_central_force(Vector3.UP * up_accel * mass)
 	_last_force_error = error
 	return is_on_ground
 
 
-func _physics_process_walking(is_on_ground: bool) -> void:
+func _physics_process_walking_force(is_on_ground: bool) -> void:
 	if not is_on_ground:
 		return
 
@@ -94,57 +104,58 @@ func _physics_process_walking(is_on_ground: bool) -> void:
 			.rotated(Vector3.UP, camera.rotation.y + TAU / 2.0)
 	)
 
-	var global_space_friction := -Vector3(
-		linear_velocity.x, 0.0, linear_velocity.z
-	) * 1600.0
-
-	var is_walking := camera_space_input.length() > 0.0
-	var force: Vector3
+	var is_walking := global_space_input.length() > 0.0
+	var accel: Vector3
 	if is_walking:
 		var walk_space := Basis.looking_at(
 			global_space_input, Vector3.UP, true
 		)
 		var walk_space_vel := walk_space.transposed() * linear_velocity
-		var walk_space_friction := (
-			walk_space.transposed() * global_space_friction
+		var forward_accel := (
+			global_space_input.length() * walk_acceleration if (
+				walk_space_vel.z < walk_speed_max
+			)
+			else -walk_space_vel.z * walk_overspeed_deceleration
 		)
-		var walk_force := (
-			global_space_input.length() * 3000.0 if walk_space_vel.z < 6.0
-			else walk_space_friction.z * 0.1
-		)
-		var walk_space_force := Vector3(
-			walk_space_friction.x,
+		var walk_space_accel := Vector3(
+			-walk_space_vel.x * ground_friction_coefficient,
 			0.0,
-			maxf(walk_space_friction.z, walk_force)
+			maxf(
+				-walk_space_vel.z * ground_friction_coefficient, forward_accel
+			)
 		)
-		force = walk_space * walk_space_force
+		accel = walk_space * walk_space_accel
 	else:
-		force = global_space_friction
+		accel = -Vector3(
+			linear_velocity.x, 0.0, linear_velocity.z
+		) * ground_friction_coefficient
 
-	apply_central_force(force)
+	apply_central_force(mass * accel)
 
 
-func _physics_process_upright_torque_pid(delta: float) -> void:
+func _physics_process_upright_force(delta: float) -> void:
 	var target := Vector3.MODEL_TOP
 	var current := global_basis.y
 	var error := current.cross(target)
-	var kp := 1000.0
-	var td := 0.2
 	var error_delta := (error - _last_upright_torque_error) / delta
-	var torque := kp * (error + td * error_delta)
-	apply_torque(torque)
+	var accel := (
+		upright_force_p_gain * error + upright_force_d_gain * error_delta
+	)
+	apply_torque(accel * get_actual_inertia())
 	_last_upright_torque_error = error
 
 
-func _physics_process_yaw_torque_pid(delta: float) -> void:
-	var target := -Vector3(camera.global_basis.z.x, 0.0, camera.global_basis.z.z).normalized()
-	var current := Vector3(global_basis.z.x, 0.0, global_basis.z.z).normalized()
+func _physics_process_turn_force(delta: float) -> void:
+	var target := -Vector3(
+		camera.global_basis.z.x, 0.0, camera.global_basis.z.z
+	).normalized()
+	var current := Vector3(
+		global_basis.z.x, 0.0, global_basis.z.z
+	).normalized()
 	var error := current.cross(target)
-	var kp := 1000.0
-	var td := 0.5
 	var error_delta := (error - _last_yaw_torque_error) / delta
-	var torque := kp * (error + td * error_delta)
-	apply_torque(torque)
+	var torque := turn_force_p_gain * error + turn_force_d_gain * error_delta
+	apply_torque(torque * get_actual_inertia())
 	_last_yaw_torque_error = error
 
 
@@ -159,7 +170,8 @@ func _input(event: InputEvent) -> void:
 		camera.rotation.y = ry
 
 
-func _update() -> void:
+func _update_tree() -> void:
+	# TODO: complete me
 	var capsule: CapsuleShape3D = collision_shape.shape
 	capsule.radius = radius
 	capsule.height = height - ground_clearance
@@ -171,3 +183,8 @@ func _update() -> void:
 	capsule_mesh.radius = radius
 	capsule_mesh.height = capsule.height
 	debug_capsule.position = collision_shape.position
+
+
+func get_actual_inertia() -> Vector3:
+	var state := PhysicsServer3D.body_get_direct_state(get_rid())
+	return state.inverse_inertia.inverse()
