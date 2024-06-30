e42bde6207e98065186453cc271a028e7fc39f4d
John Owen <john.owen@dabble.com.au>
2024-06-30 14:00:07 +1000

    tweaks

--- addons/character-controller/example/main/player.gd
+++ addons/character-controller/example/main/player.gd
@@ -1,15 +1,6 @@
 extends CharacterBody3D
 class_name Player
 
-@export var input_back_action_name := "move_backward"
-@export var input_forward_action_name := "move_forward"
-@export var input_left_action_name := "move_left"
-@export var input_right_action_name := "move_right"
-@export var input_sprint_action_name := "move_sprint"
-@export var input_jump_action_name := "move_jump"
-@export var input_crouch_action_name := "move_crouch"
-@export var input_fly_mode_action_name := "move_fly_mode"
-
 @export var underwater_env: Environment
 
 @export_group("Audio")
@@ -246,45 +237,22 @@ func _ready():
 func _physics_process(delta):
 	var is_valid_input := Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
 
+	var input_axis := Vector2.ZERO
+	var input_jump := false
+	var input_crouch := false
+	var input_sprint := false
+	var input_swim_down := false
+	var input_swim_up := false
 	if is_valid_input:
-		if Input.is_action_just_pressed(input_fly_mode_action_name):
+		if Input.is_action_just_pressed("move_fly_mode"):
 			_is_flying = not _is_flying
-		var input_axis = Input.get_vector(input_left_action_name, input_right_action_name, input_back_action_name, input_forward_action_name)
-		var input_jump = Input.is_action_just_pressed(input_jump_action_name)
-		var input_crouch = Input.is_action_pressed(input_crouch_action_name)
-		var input_sprint = Input.is_action_pressed(input_sprint_action_name)
-		var input_swim_down = Input.is_action_pressed(input_crouch_action_name)
-		var input_swim_up = Input.is_action_pressed(input_jump_action_name)
-		move(delta, input_axis, input_jump, input_crouch, input_sprint, input_swim_down, input_swim_up)
-	else:
-		# NOTE: It is important to always call move() even if we have no inputs 
-		## to process, as we still need to calculate gravity and collisions.
-		move(delta)
+		input_axis = Input.get_vector("move_left", "move_right", "move_backward", "move_forward")
+		input_jump = Input.is_action_just_pressed("move_jump")
+		input_crouch = Input.is_action_pressed("move_crouch")
+		input_sprint = Input.is_action_pressed("move_sprint")
+		input_swim_down = Input.is_action_pressed("move_crouch")
+		input_swim_up = Input.is_action_pressed("move_jump")
 
-
-func _input(event: InputEvent) -> void:
-	# Mouse look (only if the mouse is captured).
-	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		var mouse_axis: Vector2 = event.relative
-		# Horizontal mouse look.
-		actual_head_rotation.y -= mouse_axis.x * (mouse_sensitivity/1000)
-		# Vertical mouse look.
-		actual_head_rotation.x = clamp(actual_head_rotation.x - mouse_axis.y * (mouse_sensitivity/1000), -vertical_angle_limit, vertical_angle_limit)
-		
-		rotation.y = actual_head_rotation.y
-		head.rotation.x = actual_head_rotation.x
-	elif event.is_action_pressed("move_crouch"):
-		crouch_stream.play()
-	elif event.is_action_released("move_crouch"):
-		uncrouch_stream.play()
-
-
-## Call to move the character.
-## First it is defined what the direction of movement will be, whether it is vertically or not 
-## based on whether swim or fly mode is active.
-## Afterwards, the [b]move()[/b] of the base class [CharacterMovement3D] is called
-## It is then called functions responsible for head bob if necessary.
-func move(delta: float, input_axis := Vector2.ZERO, input_jump := false, input_crouch := false, input_sprint := false, input_swim_down := false, input_swim_up := false):
 	if _is_flying or _is_floating:
 		_direction_base_node = head
 	else:
@@ -370,6 +338,23 @@ func move(delta: float, input_axis := Vector2.ZERO, input_jump := false, input_c
 #		camera.set_fov(lerp(camera.fov, normal_fov, delta * fov_change_speed))
 
 	_do_head_bobbing(_horizontal_velocity, input_axis, is_sprinting, delta)
+
+
+func _input(event: InputEvent) -> void:
+	# Mouse look (only if the mouse is captured).
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		var mouse_axis: Vector2 = event.relative
+		# Horizontal mouse look.
+		actual_head_rotation.y -= mouse_axis.x * (mouse_sensitivity/1000)
+		# Vertical mouse look.
+		actual_head_rotation.x = clamp(actual_head_rotation.x - mouse_axis.y * (mouse_sensitivity/1000), -vertical_angle_limit, vertical_angle_limit)
+		
+		rotation.y = actual_head_rotation.y
+		head.rotation.x = actual_head_rotation.x
+	elif event.is_action_pressed("move_crouch"):
+		crouch_stream.play()
+	elif event.is_action_released("move_crouch"):
+		uncrouch_stream.play()
 
 
 func _do_walking(is_walking: bool, direction: Vector3, delta: float):
