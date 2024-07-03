041793c3fcdf946473cbc7e17fbc7cb8e599d349
John Owen <john.owen@dabble.com.au>
2024-07-03 13:04:41 +1000

    tweaks

--- ui/custom_button.gd
+++ ui/custom_button.gd
@@ -33,10 +33,13 @@ signal button_down
 @onready var _checkbox: Control = %Checkbox
 @onready var _check: Control = %Check
 @onready var _chevron: Control = %Chevron
+@onready var _hover: Control = %Hover
 
 
 func _ready() -> void:
 	_update()
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
 
 
 func _gui_input(event: InputEvent) -> void:
@@ -56,3 +59,11 @@ func _update() -> void:
 	_checkbox.visible = checkbox
 	_check.visible = checked
 	_chevron.visible = chevron
+
+
+func _on_mouse_entered() -> void:
+	_hover.visible = true
+
+
+func _on_mouse_exited() -> void:
+	_hover.visible = false
