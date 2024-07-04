ec4f6bdf70547160db9bb7347aff40dce0fe3630
John Owen <john.owen@dabble.com.au>
2024-07-04 16:34:57 +1000

    tweaks

--- common/main.gd
+++ common/main.gd
@@ -5,6 +5,7 @@ class_name Main
 @export var start_scene: PackedScene
 @export var game_scene: PackedScene
 var _game: Game
+@onready var _fps_counter: Label = %FpsCounter
 
 
 func _ready() -> void:
@@ -20,6 +21,15 @@ func _ready() -> void:
 	start.queue_free()
 	await start.tree_exited
 	_restart()
+
+
+func _process(_delta: float) -> void:
+	_fps_counter.text = "%s FPS " % Engine.get_frames_per_second()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("toggle_fps_counter"):
+		_fps_counter.visible = not _fps_counter.visible
 
 
 func _restart() -> void:
