8b29ee74ddd063b2115cc8f010f8c5f916bd0007
John Owen <john.owen@dabble.com.au>
2024-07-04 18:42:00 +1000

    tweaks

--- common/main.gd
+++ common/main.gd
@@ -1,23 +1,31 @@
 extends Node
 class_name Main
 
+const _FADE_DURATION = 0.15
 @export var loader_scene: PackedScene
 @export var start_scene: PackedScene
 @export var game_scene: PackedScene
 var _game: Game
+var _fade_out_started_at := -1000.0
+var _fade_in_started_at := -1000.0
+@onready var _container: Node = %Container
 @onready var _fps_counter: Label = %FpsCounter
+@onready var _transition: ColorRect = %Transition
 
 
 func _ready() -> void:
-	if not OS.is_debug_build():
+	if not OS.is_debug_build() or true:
 		var loader: Loader = loader_scene.instantiate()
-		add_child(loader)
+		_container.add_child(loader)
 		await loader.finished
+		await _fade_out()
 		loader.queue_free()
 		await loader.tree_exited
 	var start: Start = start_scene.instantiate()
-	add_child(start)
+	_container.add_child(start)
+	await _fade_in()
 	await start.menu.started
+	await _fade_out()
 	start.queue_free()
 	await start.tree_exited
 	_restart()
@@ -25,6 +33,18 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	_fps_counter.text = "%s FPS " % Engine.get_frames_per_second()
+	if Util.get_ticks_sec() - _fade_out_started_at < _FADE_DURATION:
+		_transition.color.a = smoothstep(
+			0.0,
+			1.0,
+			Util.get_ticks_sec() - _fade_out_started_at / _FADE_DURATION
+		)
+	if Util.get_ticks_sec() - _fade_in_started_at < _FADE_DURATION:
+		_transition.color.a = smoothstep(
+			0.0,
+			1.0,
+			1.0 - (Util.get_ticks_sec() - _fade_in_started_at) / _FADE_DURATION
+		)
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -34,8 +54,22 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _restart() -> void:
 	if _game:
+		await _fade_out()
 		_game.queue_free()
 		await _game.tree_exited
 	_game = game_scene.instantiate()
-	add_child(_game)
+	_container.add_child(_game)
 	_game.restarted.connect(_restart)
+	await _fade_in()
+
+
+func _fade_out() -> void:
+	_fade_out_started_at = Util.get_ticks_sec()
+	await get_tree().create_timer(_FADE_DURATION).timeout
+	_transition.color.a = 1.0
+
+
+func _fade_in() -> void:
+	_fade_in_started_at = Util.get_ticks_sec()
+	await get_tree().create_timer(_FADE_DURATION).timeout
+	_transition.color.a = 0.0
